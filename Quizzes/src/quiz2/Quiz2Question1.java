8d1ebb20d6459599331cb427799aed85037255e0
Administrator <Administrator@DESKTOP-Q3HU6Q0>
2018-09-20 16:30:14 -0400

    Final Commit Quiz 2

--- Quizzes/src/quiz2/Quiz2Question1.java
+++ Quizzes/src/quiz2/Quiz2Question1.java
@@ -1,11 +1,44 @@
 package quiz2;
 
+import java.util.Scanner;
+
 public class Quiz2Question1 {
 
+	private double carCost;
+	private double downPay;
+	private double loanLenMonths;
+	private double intRate;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner statsIn = new Scanner(System.in);
+		Quiz2Question1 statStore = new Quiz2Question1();
+		System.out.println("To calculate your monthly payment and interest payment, enter the following statistics in order, one at a time: Total Car Cost, Down Payment, Term of the loan in Months, and the total Interest Rate.");
+		statStore.carCost = statsIn.nextDouble();
+		statStore.downPay = statsIn.nextDouble();
+		statStore.loanLenMonths = statsIn.nextDouble();
+		statStore.intRate = statsIn.nextDouble();
+		statsIn.close();
+		System.out.print("Monthly payment (no rounding): " + statStore.carPaymentPerMonth() + ", Total Interest (no rounding): " + statStore.totalInt());
 
 	}
 
+	public Quiz2Question1() {
+
+	}
 	
+	public void setVars(double a, double b, double c, double d) {
+		this.carCost = a;
+		this.downPay = b;
+		this.loanLenMonths = c;
+		this.intRate = d;
+	}
+	
+	public double carPaymentPerMonth() {
+		double monthlyInt = intRate/(100*12);
+		return (carCost-downPay)*((monthlyInt*Math.pow(1+monthlyInt, loanLenMonths))/(Math.pow(1+monthlyInt, loanLenMonths)-1));
+	}
+	
+	public double totalInt() {
+		return -1*((carCost-downPay) - (this.carPaymentPerMonth() * loanLenMonths));
+	}
 }
---
+++ Quizzes/src/quiz2/Quiz2Question1Test.java
@@ -0,0 +1,41 @@
+package quiz2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import quiz2.Quiz2Question1;
+
+class Quiz2Question1Test {
+
+	@Test
+	public void carPaymentPerMonthTest() {
+		Quiz2Question1 statStore = new Quiz2Question1();
+		statStore.setVars(35000, 0, 60, 10);
+		assertTrue(statStore.carPaymentPerMonth() < 744 && statStore.carPaymentPerMonth() > 743);
+		
+		statStore.setVars(50000, 15000, 60, 10);
+		assertTrue(statStore.carPaymentPerMonth() < 744 && statStore.carPaymentPerMonth() > 743);
+		
+		statStore.setVars(30000, 15000, 15, 5);
+		assertTrue(statStore.carPaymentPerMonth() < 1034 && statStore.carPaymentPerMonth() > 1033);
+	}
+	
+	@Test
+	public void totalIntTest() {
+		Quiz2Question1 statStore = new Quiz2Question1();
+		statStore.setVars(35000, 0, 60, 10);
+		assertTrue(statStore.totalInt() < 9619 && statStore.totalInt() > 9618);
+		
+		statStore.setVars(50000, 15000, 60, 10);
+		assertTrue(statStore.totalInt() < 9619 && statStore.totalInt() > 9618);
+		
+		statStore.setVars(30000, 15000, 15, 5);
+		assertTrue(statStore.totalInt() < 505 && statStore.totalInt() > 504);
+	}
+		
+	
+	
+	
+
+}
